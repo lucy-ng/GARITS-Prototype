20ca6cd8cd4d6ec64b6dd767c9a88b68eb5977ce
lucy-ng <Lucy.Nguyen@city.ac.uk>
2022-02-10 11:45:45 +0000

    Changes to booking service form

--- src/Forms/BookingService.java
+++ src/Forms/BookingService.java
@@ -9,6 +9,22 @@ public class BookingService extends JFrame{
     private JLabel dateBookingLabel;
     private JButton submitButton;
     private JPanel mainPanel;
+    private JRadioButton MOTRadioButton;
+    private JRadioButton repairRadioButton;
+    private JLabel serviceTypeLabel;
+    private JLabel bookServiceTitle;
+    private JLabel firstNameLabel;
+    private JLabel secondNameLabel;
+    private JTextField firstName;
+    private JTextField secondName;
+    private JLabel emailLabel;
+    private JTextField email;
+    private JTextField phone;
+    private JLabel phoneLabel;
+    private JLabel addressLabel;
+    private JTextField address;
+    private JTextField postCode;
+    private JLabel postCodeLabel;
 
     public BookingService() {
         setContentPane(mainPanel);
