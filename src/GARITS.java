dbc28a05d2bc3456b754ecda1f6c5d864cb494b8
lucy-ng <Lucy.Nguyen@city.ac.uk>
2022-02-10 20:56:05 +0000

    Added forms such as login and register and update account

--- src/GARITS.java
+++ src/GARITS.java
@@ -1,10 +1,12 @@
 import Forms.*;
 
+// Creating the GARITS system
 public class GARITS {
     public GARITS() {
         BookingService bookingService = new BookingService();
     }
 
+    // Running the GARITS system
     public static void main (String[] args){
         new GARITS();
     }
