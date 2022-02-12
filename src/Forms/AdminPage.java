79a2c3d88fec173568bad47010f5b6fe6e061844
lucy-ng <Lucy.Nguyen@city.ac.uk>
2022-02-12 12:14:42 +0000

    Created Franchisee Page

---
+++ src/Forms/AdminPage.java
@@ -0,0 +1,15 @@
+package Forms;
+
+import javax.swing.*;
+
+public class AdminPage {
+    private JLabel adminPageTitle;
+    private JButton backupDatabaseButton;
+    private JButton restoreDatabaseButton;
+    private JButton addAccountButton;
+    private JButton updateAccountButton1;
+    private JButton deleteAccountButton;
+    private JLabel staffAccountsLabel;
+    private JLabel welcomeLabel;
+    private JLabel databaseLabel;
+}
---
+++ src/Forms/FranchiseePage.java
@@ -0,0 +1,14 @@
+package Forms;
+
+import javax.swing.*;
+
+public class FranchiseePage {
+    private JLabel franchiseePageTitle;
+    private JLabel customerAccountsLabel;
+    private JLabel garageLabel;
+    private JButton customerAccountsMenuButton;
+    private JButton jobsMenuButton;
+    private JButton changeDiscountPlanForButton;
+    private JButton stockControlMenuButton;
+    private JLabel welcomeLabel;
+}
