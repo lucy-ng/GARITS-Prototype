5dd410853c4115abd4bc1efa6b030120cc375bec
lucy-ng <Lucy.Nguyen@city.ac.uk>
2022-03-16 23:37:32 +0000

    added logo in homepage and changes to font colour

--- src/Forms/Homepage.java
+++ src/Forms/Homepage.java
@@ -1,15 +1,18 @@
 package Forms;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Homepage {
     private JLabel garitsTitle;
     private JButton loginButton;
     private JButton exitButton;
     private JPanel mainPanel;
+    private JLabel icon;
 
     public Homepage() {
-
+        ImageIcon logo = new ImageIcon(new ImageIcon("Images/TwoSix technologies-5.png").getImage().getScaledInstance(300,300, Image.SCALE_SMOOTH));
+        icon.setIcon(logo);
     }
 
     public JButton getLoginButton() {
