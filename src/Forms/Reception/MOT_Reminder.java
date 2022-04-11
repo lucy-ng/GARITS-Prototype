2910933b01c0c07a28a87dcbf6325c27de6ef0a2
lucy-ng <Lucy.Nguyen@city.ac.uk>
2022-04-11 11:23:04 +0100

    parts can be sold and amount is updated, payment is recorded

--- src/Forms/Reception/MOT_Reminder.java
+++ src/Forms/Reception/MOT_Reminder.java
@@ -1,24 +1,27 @@
 package Forms.Reception;
 
-import Database.Vehicle;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Vector;
 
 public class MOT_Reminder {
     private JLabel monthlyReportTitle;
     private JTextField date;
-    private JButton sendRemindersButton;
+    private JButton sendReminderButton;
     private JLabel dateLabel;
     private JLabel MOTTestRemindersLabel;
     private JPanel mainPanel;
     private JButton enterDateButton;
     private JScrollPane motScrollPane;
+    private JLabel customerUsernameLabel;
+    private JTextField username;
+    private JLabel customerEmailLabel;
+    private JTextField email;
     private JTable motTable;
 
     public MOT_Reminder() {
@@ -80,27 +83,61 @@ public class MOT_Reminder {
         enterDateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                LocalDate today = LocalDate.parse(date.getText());
+                Date sevenDays = Date.valueOf(today.plusDays(7));
+
                 try {
                     Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00/in2018t26","in2018t26","5CrmPJHN");
-                    PreparedStatement statement = connection.prepareStatement("SELECT motDate from Booking");
+                    PreparedStatement statement = connection.prepareStatement("SELECT motDate, registrationNo, CustomerAccountID from Booking WHERE motDate = ?");
+                    statement.setDate(1, sevenDays);
                     ResultSet rs = statement.executeQuery();
 
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT firstName, lastName, email FROM UserAccounts WHERE AccountID = (SELECT AccountID FROM CustomerAccount WHERE CustomerAccountID = ?)");
+
                     while (rs.next()) {
-                        PreparedStatement stmt = connection.prepareStatement("SELECT email FROM UserAccounts WHERE AccountID = (SELECT AccountID FROM CustomerAccount)");
-                        ResultSet r = stmt.executeQuery();
+                        PreparedStatement selectStmt = connection.prepareStatement("SELECT address FROM CustomerAccount WHERE CustomerAccountID = ?");
+                        selectStmt.setInt(1, rs.getInt("CustomerAccountID"));
+                        ResultSet r = selectStmt.executeQuery();
                         while (r.next()) {
-                            Date motDateText = rs.getDate("motDate");
-                            String emailText = r.getString("email");
+                            preparedStatement.setInt(1, rs.getInt("CustomerAccountID"));
+                            ResultSet resultSet = preparedStatement.executeQuery();
+                            while (resultSet.next()) {
+                                Date motDateText = rs.getDate("motDate");
+                                String regNoText = rs.getString("registrationNo");
+
+                                String firstNameText = resultSet.getString("firstName");
+                                String lastNameText = resultSet.getString("lastName");
+                                String emailText = resultSet.getString("email");
+
+                                String addressText = r.getString("address");
+
+                                Object[] row = new Object[6];
+                                for (int i = 0; i < 6; i++) {
+                                    row[0] = motDateText;
+                                    row[1] = regNoText;
+                                    row[2] = firstNameText;
+                                    row[3] = lastNameText;
+                                    row[4] = emailText;
+                                    row[5] = addressText;
+                                }
+
+                                Object[][] data =  {row};
+                                String[] columnNames = {"MOT Date", "Registration Number", "First Name", "Last Name", "Email", "Address"};
+                                JTable searchResults = new JTable(data, columnNames);
+                                motScrollPane.setViewportView(searchResults);
+                                searchResults.setVisible(true);
+                            }
                         }
                     }
                     connection.close();
                 } catch (SQLException sqlException) {
                     sqlException.printStackTrace();
                 }
+
             }
         });
 
-        sendRemindersButton.addActionListener(new ActionListener() {
+        sendReminderButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 sendEmail();
--- src/Forms/StockControl/SellParts.java
+++ src/Forms/StockControl/SellParts.java
@@ -1,11 +1,218 @@
 package Forms.StockControl;
 
+import Database.Part;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class SellParts {
 
     private JPanel mainPanel;
     private JLabel orderPartsLabel;
+    private JScrollPane partsScrollPane;
+    private JTextField searchParts;
+    private JButton searchButton;
+    private JLabel partNameLabel;
+    private JTextField partName;
+    private JLabel amountLabel;
+    private JTextField amount;
+    private JLabel priceLabel;
+    private JTextField price;
+    private JLabel totalPriceLabel;
+    private JTextField totalPrice;
+    private JLabel paymentDateLabel;
+    private JTextField paymentDate;
+    private JLabel companyNameLabel;
+    private JTextField companyName;
+    private JLabel cardTypeLabel;
+    private JTextField cardType;
+    private JLabel cardNumberLabel;
+    private JTextField cardNumber;
+    private JLabel expiryDateLabel;
+    private JTextField expiryDate;
+    private JLabel searchForSparePartsLabel;
+    private JButton sellButton;
+    private JTable partsTable;
+
+    public SellParts() {
+        partsScrollPane.setPreferredSize(new Dimension(500,500));
+
+        Vector partHeaders = new Vector();
+        partHeaders.addElement("Part Name");
+        partHeaders.addElement("Manufacturer");
+        partHeaders.addElement("Vehicle Type");
+        partHeaders.addElement("Year");
+        partHeaders.addElement("Price");
+        partHeaders.addElement("Quantity");
+        partHeaders.addElement("Low Threshold");
+        Vector partRows = new Vector();
+        partsTable = new JTable(partRows, partHeaders);
+        DefaultTableModel partsTableModel = (DefaultTableModel) partsTable.getModel();
+        partsScrollPane.setViewportView(partsTable);
+        partsTable.setVisible(true);
+
+        ArrayList<Part> partList = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00/in2018t26","in2018t26","5CrmPJHN");
+            PreparedStatement statement = connection.prepareStatement("SELECT * from SpareParts");
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String manufacturer = rs.getString("manufacturer");
+                String vehicleType = rs.getString("vehicleType");
+                String year = rs.getString("year");
+                BigDecimal price = rs.getBigDecimal("price");
+                int quantity = rs.getInt("quantity");
+                int lowThreshold = rs.getInt("lowThreshold");
+
+                Part part;
+                part = new Part(name, manufacturer, vehicleType, year, price, quantity, lowThreshold);
+                partList.add(part);
+
+                Object[] row = new Object[7];
+                for (int i = 0; i < partList.size(); i++) {
+                    row[0] = part.getName();
+                    row[1] = part.getManufacturer();
+                    row[2] = part.getVehicleType();
+                    row[3] = part.getYear();
+                    row[4] = part.getPrice();
+                    row[5] = part.getQuantity();
+                    row[6] = part.getLowThreshold();
+                }
+                partsTableModel.addRow(row);
+                fillResults();
+            }
+            connection.close();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = searchParts.getText();
+                ArrayList<Part> partList = new ArrayList<>();
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00/in2018t26","in2018t26","5CrmPJHN");
+                    PreparedStatement statement = connection.prepareStatement("SELECT name, manufacturer, vehicleType, year, price, quantity, lowThreshold from SpareParts where name = ?");
+                    statement.setString(1, text);
+                    ResultSet rs = statement.executeQuery();
+                    Part part;
+
+                    while (rs.next()) {
+                        String name = rs.getString("name");
+                        String manufacturer = rs.getString("manufacturer");
+                        String vehicleType = rs.getString("vehicleType");
+                        String year = rs.getString("year");
+                        BigDecimal price = rs.getBigDecimal("price");
+                        int quantity = rs.getInt("quantity");
+                        int lowThreshold = rs.getInt("lowThreshold");
+
+                        part = new Part(name, manufacturer, vehicleType, year, price, quantity, lowThreshold);
+                        partList.add(part);
+
+                        Object[] row = new Object[7];
+                        for (int i = 0; i < partList.size(); i++) {
+                            row[0] = part.getName();
+                            row[1] = part.getManufacturer();
+                            row[2] = part.getVehicleType();
+                            row[3] = part.getYear();
+                            row[4] = part.getPrice();
+                            row[5] = part.getQuantity();
+                            row[6] = part.getLowThreshold();
+                        }
+
+                        Object[][] data =  {row};
+                        String[] columnNames = {"Part Name", "Manufacturer", "Vehicle Type", "Year", "Price", "Quantity", "Low Threshold"};
+                        JTable searchResults = new JTable(data, columnNames);
+                        partsScrollPane.setViewportView(searchResults);
+                        searchResults.setVisible(true);
+                    }
+                    connection.close();
+                } catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+            }
+        });
+
+        sellButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00/in2018t26", "in2018t26", "5CrmPJHN");
+
+                    String companyNameText = companyName.getText();
+                    String cardTypeText = cardType.getText();
+                    String cardNumberText = cardNumber.getText();
+                    String expiryDateText = expiryDate.getText();
+                    String totalPriceText = totalPrice.getText();
+                    String paymentDateText = paymentDate.getText();
+
+                    String partNameText = partName.getText();
+                    String amountText = amount.getText();
+
+                    connection.setAutoCommit(false);
+
+                    PreparedStatement stmt = connection.prepareStatement("SELECT partID from SpareParts WHERE name = ?");
+                    stmt.setString(1, partNameText);
+                    ResultSet resultSet = stmt.executeQuery();
+
+                    while (resultSet.next()) {
+                        PreparedStatement statement = connection.prepareStatement("INSERT INTO Payments (companyName, cardType, cardNumber, expiryDate, amount, paymentDate, partID) VALUES (?,?,?,?,?,?,?)");
+                        statement.setString(1, companyNameText);
+                        statement.setString(2, cardTypeText);
+                        statement.setString(3, cardNumberText);
+                        statement.setString(4, expiryDateText);
+                        statement.setString(5, totalPriceText);
+                        statement.setString(6, paymentDateText);
+                        statement.setInt(7, resultSet.getInt("partID"));
+                        statement.executeUpdate();
+
+                        PreparedStatement selectStmt = connection.prepareStatement("SELECT quantity FROM SpareParts WHERE name = ?");
+                        selectStmt.setString(1, partNameText);
+                        ResultSet rs = selectStmt.executeQuery();
+
+                        while (rs.next()) {
+                            PreparedStatement updateStmt = connection.prepareStatement("UPDATE SpareParts SET quantity = ? WHERE name = ?");
+                            int sum = rs.getInt("quantity") - Integer.parseInt(amountText);
+                            updateStmt.setInt(1, sum);
+                            updateStmt.setString(2, partNameText);
+                            updateStmt.executeUpdate();
+                        }
+                    }
+
+                    JOptionPane.showMessageDialog(null, "Part sold!");
+                    connection.setAutoCommit(true);
+                    connection.close();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void fillResults() {
+        partsTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int selectedRow = partsTable.getSelectedRow();
+                partName.setText(partsTable.getModel().getValueAt(selectedRow, 0).toString());
+                price.setText(partsTable.getModel().getValueAt(selectedRow, 4).toString());
+            }
+        });
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
